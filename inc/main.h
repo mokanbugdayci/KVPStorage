b76161daf8ba7ef5eaf9b69addd4165e3aa53fb0
mokanbugdayci <mokanbugdayci@gmail.com>
2022-03-30 00:31:57 +0300

    files added

---
+++ inc/main.h
@@ -0,0 +1,18 @@
+/**
+ * @file main.h
+ * @author M. Okan Bugdayci
+ * @brief 
+ * @version 0.1
+ * @date 2022-03-26
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+
+
+
+#endif  //_MAIN_H_
